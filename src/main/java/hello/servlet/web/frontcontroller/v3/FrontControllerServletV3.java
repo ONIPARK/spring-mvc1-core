69fcc5a2588d7d1a656b07718330d57f9653dac5
Sion Park <onioni0000002@gmail.com>
2026-07-16 07:37:08 +0900

    Add servlet V2, V3 implementation

--- src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -1,6 +1,11 @@
-package hello.servlet.web.frontcontroller.v2;
+package hello.servlet.web.frontcontroller.v3;
 
+import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,28 +16,47 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
-public class FrontControllerServletV2 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
 
-    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
 
-    public FrontControllerServletV2() {
-        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerMap.put("/front-controller/v2/members/members", new MemberListControllerV2());
+    public FrontControllerServletV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members/members", new MemberListControllerV3());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String requestURI = request.getRequestURI();
 
-        ControllerV2 controller = controllerMap.get(requestURI);
+        ControllerV3 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        MyView view = controller.process(request, response);
-        view.render(request, response);
+
+        Map<String, String> paraMap = createParaMap(request);
+        ModelView mv = controller.process(paraMap);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+        view.render(mv.getModel(), request, response);
+
+    }
+
+    private Map<String, String> createParaMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
